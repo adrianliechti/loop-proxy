497726878e803fe2a0d1f36c949671132ac7f888
Adrian Liechti <adrian@monobox.ch>
2022-09-11 18:21:25 +0200

    introduce some flags

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"os"
 )
 
 func main() {
-	target, err := url.Parse(os.Args[1])
+	var portFlag int
+	var targetFlag string
+
+	flag.IntVar(&portFlag, "port", 8080, "lcoal proxy port")
+	flag.StringVar(&targetFlag, "target", "", "target address")
+
+	flag.Parse()
+
+	if targetFlag == "" {
+		log.Fatal("target url is required")
+	}
+
+	target, err := url.Parse(targetFlag)
 
 	if err != nil {
 		log.Fatal(err)
@@ -23,5 +36,5 @@ func main() {
 		proxy.ServeHTTP(w, r)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", portFlag), nil))
 }
